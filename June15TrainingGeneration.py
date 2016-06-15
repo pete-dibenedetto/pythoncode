c2a81dddf12aa10ceec65387a564518e66252a13
Pete DiBenedetto <peter.dibenedetto@tamr.com>
2016-06-15 17:04:32 -0400

    Add files via upload

--- June15TrainingGeneration.py
+++ June15TrainingGeneration.py
@@ -1,20 +1,20 @@
 
 # coding: utf-8
 
-# In[49]:
+# In[2]:
 
 import json
 import csv
 import sys, getopt
 
-with open('/Users/Pete/Customers/taxtest.csv') as taxfile:
+with open('/Users/Pete/Customers/doduseforjsonD.csv') as taxfile:
 
     reader = csv.DictReader(taxfile)
     
 #    print(row['first_name'], row['last_name'])
    # with open('/Users/Pete/Customers/taxpreformat.csv', 'w') as outfile:
     
-    with open('/Users/Pete/Customers/taxprejson.json', "w") as f:
+    with open('/Users/Pete/Customers/Jun15taxprejson2.json', "w") as f:
         #listWriter = csv.DictWriter(open(, 'wb'))
     
         #w = csv.DictWriter(outfile, fieldnames=reader.fieldnames)
@@ -22,10 +22,10 @@ with open('/Users/Pete/Customers/taxtest.csv') as taxfile:
         
         for index,row in enumerate(reader):
         
-            row['naic_tamr_taxdesc'] = row['naic_tamr_taxdesc'].split('-')
+            row['categoryPath'] = row['categoryPath'].split('-')
             #print(row['naic_tamr_taxdesc'])
             f.write(json.dumps(row, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
-            
+            f.write(',')
             #w.writerow(row)
     
 
